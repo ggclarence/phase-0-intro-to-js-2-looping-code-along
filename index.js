f8b4a86ab84d62afcff650769a6be922c2a81182
Eric Van <ggclarence@yahoo.com>
2022-01-24 17:00:40 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(arrayOfNames,event){
+    let newArray = []
+    for(let n = 0; n < arrayOfNames.length; n++) {
+        newArray.push(`Thank you, ${arrayOfNames[n]}, for the wonderful ${event} gift!`) 
+    }
+    return newArray
+}
+
+function countDown(count){
+    var i = 10
+    while(i>=0){
+        console.log(i--)
+    }
+}
+
+
+
